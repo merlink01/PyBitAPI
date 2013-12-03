d71db2397cad1b476879acfbfd95199572acd9c2
merlink <merlink@gmx.de>
2013-12-03 17:30:57 +0200

    New Update, added XMLRPC API, added SimpleProgramAPI (For easy communication between programs via bitessage)

--- src/class_api.py
+++ src/class_api.py
@@ -1,8 +1,15 @@
 import os
 import sys
 import time
+import json
+import threading
+import logging
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-ADDRESSVERSION = 4
+
+SUPPORTED_VERSIONS = ['0.4.1']
+
+logger = logging.getLogger('class_api')
 
 #A hack to let pybitmessage source directory exist as Subdir for testing
 if os.path.exists(os.path.abspath('PyBitmessage')):
@@ -27,7 +34,35 @@ def getAPI(workingdir=None,silent=False):
         sys.stdout = fobj    
         
     import bitmessagemain
+    version = bitmessagemain.shared.softwareVersion
+    if not version in SUPPORTED_VERSIONS:
+        sys.stderr.write('Bitmessage Vesion %s is not Supported officially.'%version)
+        
+    if version == '0.4.1':
+        ADDRESSVERSION = 4
+        
     class MainAPI(bitmessagemain.Main):
+        
+        def _close_and_wait(self):
+            self.stop()
+            while 1:
+                import time
+                time.sleep(2)
+                print self._count_threads()
+                bitmessagemain.shared.shutdown = 1
+                
+            
+        def _count_threads(self):
+
+            """This function counts running Threads"""
+
+            thcount = 0
+            threads = threading.enumerate()
+            for th in threads:
+                if th.is_alive():
+                    print th
+                    thcount += 1
+            return thcount
 
         def addToAddressBook(self, label, address):
 
@@ -39,12 +74,14 @@ def getAPI(workingdir=None,silent=False):
             if queryreturn != []:
                 raise APIError('Given Address is already inside: %s'%address)
             queryreturn = bitmessagemain.shared.sqlExecute('''INSERT INTO addressbook VALUES (?,?)''',label, address)
+            return True
 
         def addSubscription(self, label, address):
             
             '''Add a Subscription
             Usage: api.addSubscription(label,bmaddressl)'''
             
+            logger.info('Label: %s Address: %s'%(label, address))
             unicode(label, 'utf-8')
             address = bitmessagemain.addBMIfNotPresent(address)
             status, addressVersionNumber, streamNumber, toRipe = bitmessagemain.decodeAddress(address)
@@ -57,22 +94,30 @@ def getAPI(workingdir=None,silent=False):
                 raise APIError('AlreadySubscribedError')
             bitmessagemain.shared.sqlExecute('''INSERT INTO subscriptions VALUES (?,?,?)''',label, address, True)
             bitmessagemain.shared.reloadBroadcastSendersForWhichImWatching()
+            return True
+
 
         def addToBlacklist(self, label, address, enabled=True):
             
             '''Add a Bitmessage Address to the Blacklist
             Usage: api.addToBlacklist(label,bmaddress,enabled[True,False])'''
             
+            logger.info('Label: %s Address: %s Enabled: %s'%(label, address, enabled))
             unicode(label, 'utf-8')
             bitmessagemain.shared.sqlExecute('''INSERT INTO blacklist VALUES (?,?,?)''',label,address,enabled)
+            return True
+
 
         def addToWhitelist(self, label, address, enabled=True):
             
             '''Add a Bitmessage Address to the Whitelist
             Usage: api.addToBlacklist(label,bmaddress,enabled[True,False])'''
             
+            logger.info('Label: %s Address: %s Enabled: %s'%(label, address, enabled))
             unicode(label, 'utf-8')
             bitmessagemain.shared.sqlExecute('''INSERT INTO whitelist VALUES (?,?,?)''',label,address,enabled)
+            return True
+
 
         def clientStatus(self):
             
@@ -102,6 +147,7 @@ def getAPI(workingdir=None,silent=False):
             info['numberOfBroadcastsProcessed'] = bitmessagemain.shared.numberOfBroadcastsProcessed
             info['numberOfPubkeysProcessed'] = bitmessagemain.shared.numberOfPubkeysProcessed
             info['networkStatus'] = networkStatus
+            logger.debug('Status: %s'%info)
             return info
 
         def createDeterministicAddresses(self,passphrase,label='',numberOfAddresses=1,eighteenByteRipe=False,totalDifficulty=1,smallMessageDifficulty=1,streamNumberForAddress=1):
@@ -109,6 +155,7 @@ def getAPI(workingdir=None,silent=False):
             '''Creates a Deterministic Bitmessage Address (an Address based on a password)
             Usage: api.createDeterministicAddresses(passphrase,label,numberOfAddresses,eighteenByteRipe,totalDifficulty,smallMessageDifficulty)'''
             
+            logger.debug('Passphrase: %s Label: %s'%(passphrase,label))
             if len(passphrase) == 0:
                 raise APIError('The specified passphrase is blank.')
             
@@ -171,17 +218,20 @@ def getAPI(workingdir=None,silent=False):
                 bitmessagemain.shared.config.write(configfile)
             
             bitmessagemain.shared.reloadMyAddressHashes()
+            return True
 
         def deleteChannel(self,address):
             self.deleteAddress(address)
             self.deleteFromAddressBook(address)
-
+            return True
+            
         def deleteFromAddressBook(self,address):
 
             '''Delete a Contact from Address Book
             Usage: api.deleteContact(bmaddress)'''
 
             queryreturn = bitmessagemain.shared.sqlExecute('''delete from addressbook where address=?''',address)
+            return True
 
         def deleteFromBlacklist(self,address):
             
@@ -189,6 +239,7 @@ def getAPI(workingdir=None,silent=False):
             Usage: api.deleteFromBlacklist(bmaddress)'''
             
             bitmessagemain.shared.sqlExecute('''delete from blacklist where address=?''',address)
+            return True
             
         def deleteFromWhitelist(self,address):
             
@@ -196,6 +247,7 @@ def getAPI(workingdir=None,silent=False):
             Usage: api.deleteFromWhitelist(bmaddress)'''
             
             bitmessagemain.shared.sqlExecute('''delete from whitelist where address=?''',address)
+            return True
 
         def deleteSubscription(self,address):
 
@@ -205,6 +257,7 @@ def getAPI(workingdir=None,silent=False):
             address = bitmessagemain.addBMIfNotPresent(address)
             bitmessagemain.shared.sqlExecute('''DELETE FROM subscriptions WHERE address=?''',address)
             bitmessagemain.shared.reloadBroadcastSendersForWhichImWatching()
+            return True
 
         def getAllInboxMessageIDs(self):
             
@@ -471,6 +524,8 @@ def getAPI(workingdir=None,silent=False):
             
             msgid = msgid.decode('hex')
             bitmessagemain.shared.sqlExecute('''UPDATE inbox SET read='1' WHERE msgid=?''', msgid) 
+            return True
+            
             
         def markInboxMessageAsUnread(self,msgid):
             
@@ -478,7 +533,8 @@ def getAPI(workingdir=None,silent=False):
             Usage: api.markInboxMessageAsUnread()'''
             
             msgid = msgid.decode('hex')
-            bitmessagemain.shared.sqlExecute('''UPDATE inbox SET read='0' WHERE msgid=?''', msgid) 
+            bitmessagemain.shared.sqlExecute('''UPDATE inbox SET read='0' WHERE msgid=?''', msgid)
+            return True
 
         def sendBroadcast(self,fromAddress,subject,message):
             
@@ -555,6 +611,7 @@ def getAPI(workingdir=None,silent=False):
                 raise APIError('WrongValueGivenError:%s'%value)
 
             bitmessagemain.shared.config.set('bitmessagesettings', 'blackwhitelist', value)
+            return True
             
         def trashInboxMessage(self,msgid):
             
@@ -563,7 +620,8 @@ def getAPI(workingdir=None,silent=False):
 
             msgid = msgid.decode('hex')
             bitmessagemain.helper_inbox.trash(msgid)
-
+            return True
+            
         def trashSentMessage(self,msgid):
 
             '''Trash a Message from Outbox by a given ID
@@ -571,6 +629,7 @@ def getAPI(workingdir=None,silent=False):
 
             msgid = msgid.decode('hex')
             bitmessagemain.shared.sqlExecute('''UPDATE sent SET folder='trash' WHERE msgid=?''',msgid)
+            return True
             
         def _verifyAddress(self, address):
             status, addressVersionNumber, streamNumber, ripe = bitmessagemain.decodeAddress(address)
@@ -596,159 +655,79 @@ def getAPI(workingdir=None,silent=False):
     time.sleep(5)
     return api
 
-import unittest
-class TestFeed(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(cls):
-        print 'Testing PyBitApi, Errors in class_singleWorker.py should be fixed: https://github.com/Bitmessage/PyBitmessage/issues/541'
-        global path
-        import tempfile
-        path = tempfile.mkdtemp()
+
+class XMLRPCServer:
+    def __init__(self,path=None):
+        self.path = path
+        self.quit = False
         
-        global api
-        api = getAPI(workingdir=path)
-        import time
-        time.sleep(5)
+    def start(self,address='localhost',port=9000):
+        if self.path:
+            self.api = getAPI(self.path)
+        else:
+            self.api = class_api.getAPI()
+        self.server = SimpleXMLRPCServer((address, port),logRequests=False)
+        self.server.register_instance(self.api)
+        server_thread = threading.Thread(target=self._work_loop)
+        server_thread.start()
         
-    @classmethod
-    def tearDownClass(cls):
-        import shutil
-        import time
-        api.stop()
-        time.sleep(10)
-        try:
-            shutil.rmtree(path)
-        except:
-            pass
+    def _work_loop(self):
+        while not self.quit:
+            self.server.handle_request()
+        print 'Server exits'
+        self.api.stop()
+
+    def stop(self):
+        self.quit = True
+
+class SimpleApi:
+    def __init__(self,name,path=None):
+        self.name = name
+        
+        if not path:
+            path = os.path.abspath('bm_working_dir')
+            try:
+                os.makedirs(path)
+            except:
+                pass
             
-    def test_00_delete_standard_subscription(self):
-        api.deleteSubscription(api.listSubscriptions()[0]['address'])
-        assert len(api.listSubscriptions())==0
+        self.api = getAPI(workingdir=path,silent=True)
+
+        for entry in self.api.listAddresses():
+            if entry['label'] != name:
+                self.api.deleteAddress(entry['address'])
+
+        if len(self.api.listAddresses()) == 0:
+            self.api.createRandomAddress(name)
         
+        while len(self.api.listAddresses()) == 0:
+            time.sleep(0.5)
             
-    def test_01_addresses(self):
-        api.createRandomAddress('a')
-        assert api.listAddresses()[0]['label'] == 'a',api.listAddresses()[0]
-        api.createDeterministicAddresses('b')
-        assert api.listAddresses()[1]['label'] == 'b',api.listAddresses()[1]
+        self.address = self.api.listAddresses()[0]['address']
         
-        api.deleteAddress(api.listAddresses()[0]['address'])
-        api.deleteAddress(api.listAddresses()[0]['address'])
-        assert api.listAddresses() == [],api.listAddresses()
+    def get_address(self):
+        return self.address
 
-    def test_02_subscriptions(self):
-        api.addSubscription('a','BM-2D9vJkoGoTBhqMyZyjvELKgBWFMr6iGCQQ')
-        assert api.listSubscriptions()[0]['label'] == 'a',api.listSubscriptions()[0]
-        api.deleteSubscription(api.listSubscriptions()[0]['address'])
-        assert len(api.listSubscriptions()) == 0,api.listSubscriptions()
+    def get_header(self):
+        return 'Program:%s'%self.name
 
-    def test_03_channels(self):
-        api.joinChannel('general')
-        assert api.listAddresses()[0]['label'] == '[chan] general',api.listAddresses()[0]
-        assert api.listAddressBook()[0]['label'] == '[chan] general',api.listAddressBook()[0]
-        api.deleteChannel(api.listAddresses()[0]['address'])
-        assert len(api.listAddresses()) == 0 
-        assert len(api.listAddressBook()) == 0 
+    def send_data(self,ToAddress,data):
+        data = json.dumps(data)
+        return self.api.sendMessage(self.address,ToAddress,self.get_header(),data)
         
-    def test_04_manage_addressbook(self):
-        api.addToAddressBook('a','a')
-        assert api.listAddressBook()[0]['address'] == 'a',api.listContacts()[0]
-        count = len(api.listAddressBook())
-        api.deleteFromAddressBook('a')
-        assert len(api.listAddressBook()) == 0
+    def send_data_wait(self,ToAddress,data):
+        data = json.dumps(data)
+        ack = self.api.sendMessage(self.address,ToAddress,self.get_header(),data)
+        while not 'msgsent' in self.api.getSentMessageByAckData(ack)[0]['status']:
+            time.sleep(1)
 
-    def test_05_manage_blackwhitelist(self):
-        assert api.getBlackWhitelist() == 'black'
-        api.setBlackWhitelist('white')
-        assert api.getBlackWhitelist() == 'white'
-        api.setBlackWhitelist('black')
-        assert api.getBlackWhitelist() == 'black'
+    def get_data(self):
+        out = []
+        for message in self.api.getAllInboxMessages():
+            sys.stderr.write(str(message)) 
+            if message['subject'] == self.get_header():
+                out.append(json.loads(message['message']))
+        return out
         
-        api.addToBlacklist('a','a')
-        assert api.listBlacklist()[0]['label'] == 'a'
-        api.deleteFromBlacklist('a')
-        assert api.listBlacklist() == []
-        
-        api.addToWhitelist('a','a')
-        assert api.listWhitelist()[0]['label'] == 'a'
-        api.deleteFromWhitelist('a')
-        assert api.listWhitelist() == []
-
-    def test_06_send_messages(self):
-        addr = api.createRandomAddress('sendtest')
-        api.addSubscription('a','BM-2D9vJkoGoTBhqMyZyjvELKgBWFMr6iGCQQ')
-        ackdata1 = api.sendMessage(addr,'BM-2D9vJkoGoTBhqMyZyjvELKgBWFMr6iGCQQ','apitest','apitest\nhttps://github.com/merlink01/PyBitAPI')
-        ackdata2 = api.sendBroadcast(addr,'apitest','apitest')
-        while api.getSentMessageByAckData(ackdata1) == 'notfound':
-            pass
-        while api.getSentMessageByAckData(ackdata2) == 'notfound':
-            pass
-            
-        assert api.getSentMessageByAckData(ackdata1)[0]['status'] in ['msgqueued', 'broadcastqueued', \
-        'broadcastsent', 'doingpubkeypow', 'awaitingpubkey', 'doingmsgpow', 'forcepow', 'msgsent', \
-        'msgsentnoackexpected', 'ackreceived'],api.getSentMessageByAckData(ackdata1)
-        assert api.getSentMessageByAckData(ackdata2)[0]['status'] in ['msgqueued', 'broadcastqueued', \
-        'broadcastsent', 'doingpubkeypow', 'awaitingpubkey', 'doingmsgpow', 'forcepow', 'msgsent', \
-        'msgsentnoackexpected', 'ackreceived'],api.getSentMessageByAckData(ackdata2)
-
-    def test_07_manage_sent_messages(self):
-        addr = api.createRandomAddress('sendtest')
-        ackdata = api.sendMessage(addr,addr,'last','last')
-        idnum = api.getSentMessageByAckData(ackdata)[0]['msgid']
-        
-        assert api.getAllSentMessages() != []
-        assert api.getAllSentMessageIDs() != []
-        assert api.getSentMessagesBySender(addr) != []
-        assert api.getSentMessageByAckData(ackdata) != []
-        assert api.getSentMessageByID(idnum) != []
-        assert api.getSentMessageStatus(ackdata) in ['msgqueued'],api.getSentMessageStatus(ackdata)
-
-        api.trashSentMessage(idnum)
-        messages = api.getAllSentMessages()
-        for message in messages:
-            assert message['msgid'] != idnum
-    
-    def test_08_manage_inbox_messages(self):
-        assert api.clientStatus > 0, 'Not connected'
-        addr = api.createRandomAddress('sendtest')
-        counter = 0
-        ackdata = api.sendMessage(addr,addr,'test','test')
-        while api.getAllInboxMessages() == []:
-            if counter > 20:
-                ackdata = api.sendMessage(addr,addr,'apitest','apitest\nhttps://github.com/merlink01/PyBitAPI')
-            time.sleep(10)
-            assert api.clientStatus > 0, 'Not connected'
-            
-        assert api.getAllInboxMessages() != [],api.getAllInboxMessages()
-        assert api.getAllInboxMessageIDs() != [],api.getAllInboxMessageIDs()
-        
-        msgid = api.getAllInboxMessages()[0]['msgid']
-        recv = api.getAllInboxMessages()[0]['toAddress']
-        assert api.getInboxMessageByID(msgid) != []
-        assert api.getInboxMessagesByReceiver(recv) != []
-        
-        assert api.getInboxMessageByID(msgid)[0]['read'] == 0,api.getInboxMessageByID(msgid)[0]['read']
-        api.markInboxMessageAsRead(msgid)
-        assert api.getInboxMessageByID(msgid)[0]['read'] == 1,api.getInboxMessageByID(msgid)[0]['read']
-        api.markInboxMessageAsUnread(msgid)
-        assert api.getInboxMessageByID(msgid)[0]['read'] == 0,api.getInboxMessageByID(msgid)[0]['read']
-        
-        api.trashInboxMessage(msgid)
-        messages = api.getAllInboxMessages()
-        for msg in messages:
-            assert msg['msgid'] != msgid
-        
-        api.getAllInboxMessages()
-        
-if __name__ == "__main__":
-    import logging,os,sys
-    logger = logging.getLogger()
-    fmt_string = "[%(levelname)-7s]%(asctime)s.%(msecs)-3d\
-    %(module)s[%(lineno)-3d]/%(funcName)-10s  %(message)-8s "
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter(fmt_string, "%H:%M:%S"))
-    logger.addHandler(handler)
-    logger.setLevel(logging.WARNING)
-    
-    unittest.main()
+    def close(self):
+        self.api.stop()
